fdfe2fbc911e75ab08c2f944ad3a49879d583869
Andrey Aleksandrov <melifaro13@yandex.ru>
2023-01-12 21:55:28 +0300

    fix: внесены правки первое ревью

--- scripts/index.js
+++ scripts/index.js
@@ -9,27 +9,27 @@ let jobInput = document.querySelector('.popup__info_type_job');
 
 function openPopUp() {
     popUp.classList.add('popup_opened');
-}
-popUpEdit.addEventListener('click', openPopUp);
+} //эта функция открывает popUp
 
 function closePopUp() {
     popUp.classList.remove('popup_opened');
-}
-popUpClose.addEventListener('click', closePopUp);
+} //эта функция закрывает popUp
 
 function infoPopUp() {
     nameInput.value = nameTitle.textContent;
     jobInput.value = jobTitle.textContent;
     openPopUp();
-}
-popUpEdit.addEventListener('click', infoPopUp);
+} //эта функция выводит данные Name и Job при открытии popUp
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
     nameTitle.textContent = nameInput.value;
     jobTitle.textContent = jobInput.value;
     closePopUp();
-}
+} //эта функция сохраняет данные Name и Job при редактировании
+
+popUpClose.addEventListener('click', closePopUp);
+popUpEdit.addEventListener('click', infoPopUp);
 formElement.addEventListener('submit', handleFormSubmit);
 
 
