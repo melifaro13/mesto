62ab7c8b608da481ced0824d9902fb01ca86d88a
Andrey Aleksandrov <melifaro13@yandex.ru>
2023-01-15 21:14:14 +0300

    feat: теперь можно активировать лайки

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,7 @@ let popUp = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__form')
 let nameInput = document.querySelector('.popup__info_type_name');
 let jobInput = document.querySelector('.popup__info_type_job');
+let likeImage = document.querySelectorAll('[class="element__like"]');
 
 function openPopUp() {
     popUp.classList.add('popup_opened');
@@ -28,9 +29,20 @@ function handleFormSubmit (evt) {
     closePopUp();
 } //эта функция сохраняет данные Name и Job при редактировании
 
-popUpClose.addEventListener('click', closePopUp);
-popUpEdit.addEventListener('click', infoPopUp);
-formElement.addEventListener('submit', handleFormSubmit);
+popUpClose.addEventListener('click', closePopUp); //слушатель закрывает попап
+popUpEdit.addEventListener('click', infoPopUp); //слушатель открывает попап
+formElement.addEventListener('submit', handleFormSubmit); //слушатель сохраняет вводимые данные
 
+function closePopUpEsc(e) {
+    if (e.key === "Escape") {
+        closePopUp();
+    }
+} //эта функция для закрытия popUp с помощью клавиши 'Esc'
 
+document.addEventListener('keydown', closePopUpEsc); //этот слушатель закрывает popUp, при нажатии на кнопку 'Esc'
 
+likeImage.forEach(function(item) {
+    item.addEventListener('click', function () {
+        this.classList.toggle('element__like_active')
+    });
+}); //активация лайков через forEach
