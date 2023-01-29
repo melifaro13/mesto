68df3c57a282c200e51000f07b02036ccc107755
Andrey Aleksandrov <melifaro13@yandex.ru>
2023-01-29 19:30:59 +0300

    fix: исправлены недочеты

--- scripts/index.js
+++ scripts/index.js
@@ -30,8 +30,8 @@ const initialCards = [
 //Постоянные редактирования профиля
 
 const popupEditProfile = document.querySelector('.popup_type_edit-profile');
-const popupCloseProfile = popupEditProfile.querySelector('.popup__closed');
-const popupEdit = document.querySelector('.profile__edit-button');
+const profileCloseButton = popupEditProfile.querySelector('.popup__closed');
+const popupEditButton = document.querySelector('.profile__edit-button');
 const nameTitle = document.querySelector('.profile__name');
 const jobTitle = document.querySelector('.profile__information');
 const formProfile = popupEditProfile.querySelector('.popup__form')
@@ -41,8 +41,8 @@ const jobInput = document.querySelector('.popup__info_type_job');
 //Постоянные добавления карточек
 
 const popupAddCard = document.querySelector('.popup_type_add-card');
-const popupCloseAddCard = popupAddCard.querySelector('.popup__closed');
-const popupAdd = document.querySelector('.profile__add-button');
+const addCardCloseButton = popupAddCard.querySelector('.popup__closed');
+const popupAddCardButton = document.querySelector('.profile__add-button');
 const formElement = popupAddCard.querySelector('.popup__form');
 const placeInput = document.querySelector('.popup__info_type_place');
 const linkInput = document.querySelector('.popup__info_type_link');
@@ -50,7 +50,7 @@ const linkInput = document.querySelector('.popup__info_type_link');
 //Постоянные просмотра карточек
 
 const popupShowCard = document.querySelector('.popup_type_img');
-const popupCloseShowCard = popupShowCard.querySelector('.popup__closed');
+const showCardCloseButton = popupShowCard.querySelector('.popup__closed');
 const imgLink = document.querySelector('.popup__img');
 const imgCaption = document.querySelector('.popup__caption');
 
@@ -61,21 +61,21 @@ const elementTemplate = document.querySelector('#element-template').content;
 const elementsList = document.querySelector('.elements');
 
 //эта функция открывает popup
-function openPopUp(popup) {
+function openPopup(popup) {
     popup.classList.add('popup_opened');
 }
 
 //эта функция закрывает popup
-function closePopUp(popup) {
+function closePopup(popup) {
     popup.classList.remove('popup_opened');
 }
 
 
 //эта функция выводит данные Name и Job при открытии popup
-function infoPopUp() {
+function openProfilePopup() {
     nameInput.value = nameTitle.textContent;
     jobInput.value = jobTitle.textContent;
-    openPopUp(popupEditProfile);
+    openPopup(popupEditProfile);
 }
 
 //эта функция сохраняет данные Name и Job при редактировании
@@ -83,7 +83,7 @@ function handleFormProfileSubmit (evt) {
     evt.preventDefault();
     nameTitle.textContent = nameInput.value;
     jobTitle.textContent = jobInput.value;
-    closePopUp(popupEditProfile);
+    closePopup(popupEditProfile);
 }
 
 // эта функция открывает картинку
@@ -91,20 +91,20 @@ function openImage(link, name){
     imgCaption.textContent = name;
     imgLink.src = link;
     imgLink.alt = name;
-    openPopUp(popupShowCard);
+    openPopup(popupShowCard);
 }
 
 // эта функция создает элемент с картинкой
 function createElement (link, name) {
     const elementContent = elementTemplate.querySelector('.element').cloneNode(true);
     const elementImg = elementContent.querySelector('.element__img');
-    const elementDelete = elementContent.querySelector('.element__delete');
+    const elementDeleteButton = elementContent.querySelector('.element__delete');
     const elementTitle = elementContent.querySelector('.element__title');
     const elementLike = elementContent.querySelector('.element__like');
     elementImg.src = link;
     elementImg.alt = name;
     elementTitle.textContent = name;
-    elementDelete.addEventListener('click', () => elementContent.remove());
+    elementDeleteButton.addEventListener('click', () => elementContent.remove());
     elementLike.addEventListener('click', () => {
       elementLike.classList.toggle('element__like_active');
     });
@@ -117,48 +117,48 @@ function handleFormElementSubmit (evt) {
     evt.preventDefault();
     const newElement = createElement(linkInput.value, placeInput.value)
     elementsList.prepend(newElement);
-    closePopUp(popupAddCard);
+    closePopup(popupAddCard);
     evt.target.reset();
 }
 
 //достаем картинки из массива
-function addElement () {
+function addElements () {
     const elements = initialCards.map(function (item) {
         const element = createElement(item.link, item.name);
         return element;
     });
     elementsList.append(...elements);
 }
-addElement();
+addElements();
 
 //слушатель открывает модальное окно редактирования профиля
-popupEdit.addEventListener('click', () => infoPopUp(popupEditProfile));
+popupEditButton.addEventListener('click', () => openProfilePopup(popupEditProfile));
 //слушатель закрывает модальное окно редактирования профиля
-popupCloseProfile.addEventListener('click', () => closePopUp(popupEditProfile));
+profileCloseButton.addEventListener('click', () => closePopup(popupEditProfile));
 //слушатель сохраняет вводимые данные в модальное окно
 formProfile.addEventListener('submit', handleFormProfileSubmit);
 
 //Добавление и удаление карточек
 
 //слушатель открывает модальное окно добавления карточек
-popupAdd.addEventListener('click', () => openPopUp(popupAddCard));
+popupAddCardButton.addEventListener('click', () => openPopup(popupAddCard));
 //слушатель закрывает модальное окно добавления карточек
-popupCloseAddCard.addEventListener('click', () => closePopUp(popupAddCard));
+addCardCloseButton.addEventListener('click', () => closePopup(popupAddCard));
 //слушатель добавляет новый элемент
 formElement.addEventListener('submit', handleFormElementSubmit);
 
 //слушатель закрывает модальное окно картинки
-popupCloseShowCard.addEventListener('click', () => closePopUp(popupShowCard));
+showCardCloseButton.addEventListener('click', () => closePopup(popupShowCard));
 
 
 //Эксперименты
 
 //эта функция для закрытия popUp с помощью клавиши 'Esc'
-function closePopUpEsc(e) {
+function closePopupEsc(e) {
     if (e.key === "Escape") {
-        closePopUp(popupEditProfile);
+        closePopup(popupEditProfile);
     }
 }
 
-document.addEventListener('keydown', closePopUpEsc); //этот слушатель закрывает popUp, при нажатии на кнопку 'Esc'
+document.addEventListener('keydown', closePopupEsc); //этот слушатель закрывает popUp, при нажатии на кнопку 'Esc'
 
